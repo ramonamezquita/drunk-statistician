fadf4bbee7eb311a1c82b2fe86e2f5c25b57ab95
Ramón Amézquita <ramon.amez@kuona.ai>
2024-03-18 21:44:53 -0700

    Refactoring cards grid

--- src/data/cardsData.js
+++ src/data/cardsData.js
@@ -1,17 +1,29 @@
 const cardsData = [
     {
         title: "The drunk statistician",
-        description: "See how truthful a drunk statistician can be."
+        description: "See how wise a drunk statistician can be.",
+        imagePath: "/src/assets/drunk.png",
+        altText: ""
     },
     {
         title: "Visualizing the loss landscape",
-        description: "A recap on different techniques to further visualize the loss landscape."
+        description: "A recap on different techniques to further visualize the loss landscape.",
+        imagePath: "/src/assets/loss_landscape.png",
+        altText: ""
     },
     {
         title: "PINNs for demand forecasting",
-        description: "Discover how to embed economic theory to your demand forecasting models."
+        description: "Discover how to embed economic theory to your demand forecasting models.",
+        imagePath: "/src/assets/pinns.png",
+        altText: ""
     },
-    
+    {
+        title: "FastAPI + USE",
+        description: "Learn how to set up a Universal Sentence Encoder inference service.",
+        imagePath: "/src/assets/cat.png",
+        altText: ""
+    }
+
 ]
 
 export default cardsData;
